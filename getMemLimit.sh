0da17a09d15a48a6dd6d7ca548daf45d3bf4f26c
Joao Cristiano M Silva <jcmonsilv@gmail.com>
2021-01-06 17:18:49 -0300

    Check one more file

--- getMemLimit.sh
+++ getMemLimit.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 var=$1
-echo $(( $( cat /sys/fs/cgroup/memory.max  ) * $var / 100  ))
+if [[ -e /sys/fs/cgroup/memory.max ]]; then
+  echo $(( $( cat /sys/fs/cgroup/memory.max  ) * $var / 100  ))
+elif [[ -e /sys/fs/cgroup/memory/memory.limit_in_bytes  ]]; then
+  echo $(( $( cat /sys/fs/cgroup/memory/memory.limit_in_bytes  ) * $var / 100  ))
+fi
